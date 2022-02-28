06e4c6bdb85c61c6a086a3fa73a8ea4eeac3945c
davmz <montanezdavid@gmail.com>
2022-02-27 23:19:10 -0600

    Generated Custom Routing to Anime and Type SHOW/INDEX (action and view) Controllers

--- app/controllers/animes_controller.rb
+++ app/controllers/animes_controller.rb
@@ -1,7 +1,10 @@
 class AnimesController < ApplicationController
   def index
+    # @animes = Anime.order("score DESC"); -> creates the famous n+1 problem
+    @animes = Anime.includes(:type).all.order("score DESC");
   end
 
   def show
+    @anime = Anime.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  get 'types/index'
-  get 'types/show'
-  get 'animes/index'
-  get 'animes/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # get 'types/index'
+  # get 'types/show'
+  # get 'animes/index'
+  # get 'animes/show'
+
+  resources :animes, only: [:index, :show]
+  resources :types, only: [:index, :show]
 end
