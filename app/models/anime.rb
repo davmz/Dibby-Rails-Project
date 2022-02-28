03f01fb958c39511e693cb010f4a41a68dc23a35
davmz <montanezdavid@gmail.com>
2022-02-27 22:55:35 -0600

    Populated Type and Anime Models with the CSV

--- app/models/anime.rb
+++ app/models/anime.rb
@@ -2,7 +2,7 @@ class Anime < ApplicationRecord
   belongs_to :type
 
   # validation
-  validates :name, :episode, :status, :season, :duration, :rating, :score, :vote, :description, presence :true
+  validates :name, :episode, :status, :season, :duration, :rating, :score, :vote, :description, presence: true
   validates :name, uniqueness: true
   validates :vote, numericality: { only_integer: true }
   validates :score, numericality: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require "csv"
+
+Anime.delete_all
+Type.delete_all
+
+filename = Rails.root.join("db/dataanime.csv")
+puts "Loading Anime the CSV file: #{filename}"
+
+csv_data = File.read(filename)
+animes = CSV.parse(csv_data, headers:true, encoding: "utf-8")
+
+animes.each do | a |
+  type = Type.find_or_create_by(name: a["Type"])
+
+  if type && type.valid?
+
+    anime = type.animes.create(
+        name: a["Title"],
+        episode: a["Episodes"],
+        status: a["Status"],
+        season: a["Starting season"],
+        duration: a["Duration"],
+        rating: a["Rating"],
+        score: a["Score"],
+        vote: a["Scored by"],
+        description: a["Description"]
+    )
+
+    puts "Invalid anime #{a["Title"]}" unless anime.valid?
+  else
+    puts "Invalid TYPE #{a["Type"]} for anime #{a["Title"]}."
+  end
+end
+
+puts "Created #{Type.count} Types"
+puts "Created #{Anime.count} Animes"
