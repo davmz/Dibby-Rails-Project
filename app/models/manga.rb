af4bb0182ccb03e6e425cb9365f60b63924c670a
davmz <montanezdavid@gmail.com>
2022-02-28 17:00:05 -0600

    Database migration for new Model and Validation for Manga Model

--- app/models/manga.rb
+++ app/models/manga.rb
@@ -1,3 +1,8 @@
 class Manga < ApplicationRecord
   belongs_to :media_type
+
+  # validation
+  validates :name, :chapter, :synopsis, :volumes, :status, presence: true
+  validates :name, uniqueness: true
+  validates :chapter, :popularity, :volumes, numericality: { only_integer: true }
 end
