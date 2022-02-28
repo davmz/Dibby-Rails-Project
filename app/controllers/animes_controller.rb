8df27993da8f6b726c818d32fab531af8cb434fe
davmz <montanezdavid@gmail.com>
2022-02-27 23:56:04 -0600

    Modified and added visualizations to Anime and Type on VIEWS and CONTROLLERS

--- app/controllers/animes_controller.rb
+++ app/controllers/animes_controller.rb
@@ -1,7 +1,7 @@
 class AnimesController < ApplicationController
   def index
     # @animes = Anime.order("score DESC"); -> creates the famous n+1 problem
-    @animes = Anime.includes(:type).all.order("score DESC");
+    @animes = Anime.includes(:type).all.order("score DESC")
   end
 
   def show
--- app/controllers/types_controller.rb
+++ app/controllers/types_controller.rb
@@ -1,7 +1,9 @@
 class TypesController < ApplicationController
   def index
+    @types = Type.all
   end
 
   def show
+    @type = Type.find(params[:id])
   end
 end
