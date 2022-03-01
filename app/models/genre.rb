fa7c16a5ebf0cc01c736172b1ae5ca8f1d11a8e3
davmz <montanezdavid@gmail.com>
2022-03-01 01:39:15 -0600

    Edited and added new relationships on Genre and Manga model relationship

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,6 +2,9 @@ class Genre < ApplicationRecord
   has_many :anime_genres
   has_many :animes, through: :anime_genres
 
+  has_many :manga_genres
+  has_many :mangas, through: :manga_genres
+
   # validation
   validates :name, presence: true, uniqueness: true
 end
--- app/models/manga.rb
+++ app/models/manga.rb
@@ -1,5 +1,7 @@
 class Manga < ApplicationRecord
   belongs_to :media_type
+  has_many :manga_genres
+  has_many :genres, through: :manga_genres
 
   # validation
   ## There can be the same name as they might be different
@@ -7,4 +9,8 @@ class Manga < ApplicationRecord
   ## light novel and the other one a manga).
   validates :name, :chapter, :volumes, :status, presence: true
   validates :chapter, :popularity, :volumes, numericality: { only_integer: true }
+
+  def manga_genres_list
+    self.genres.map(&:name).join(", ")
+  end
 end
