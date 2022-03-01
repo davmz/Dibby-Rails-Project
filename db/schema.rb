deb7a524c463dd8535223b37ecbb31094a1b77ab
davmz <montanezdavid@gmail.com>
2022-03-01 01:36:42 -0600

    Generated new Manga Genre model to join many to many relationship between Manga and Genre

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_28_225546) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_01_073541) do
   create_table "anime_genres", force: :cascade do |t|
     t.integer "anime_id", null: false
     t.integer "genre_id", null: false
@@ -42,6 +42,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_28_225546) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "manga_genres", force: :cascade do |t|
+    t.integer "manga_id", null: false
+    t.integer "genre_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["genre_id"], name: "index_manga_genres_on_genre_id"
+    t.index ["manga_id"], name: "index_manga_genres_on_manga_id"
+  end
+
   create_table "mangas", force: :cascade do |t|
     t.string "name"
     t.integer "chapter"
@@ -71,5 +80,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_28_225546) do
   add_foreign_key "anime_genres", "animes"
   add_foreign_key "anime_genres", "genres"
   add_foreign_key "animes", "types"
+  add_foreign_key "manga_genres", "genres"
+  add_foreign_key "manga_genres", "mangas"
   add_foreign_key "mangas", "media_types"
 end
