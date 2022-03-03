0a66f8f639ae57b52995f42f885a908e0115074d
davmz <montanezdavid@gmail.com>
2022-03-02 18:57:23 -0600

    Navigation styling modified search bar

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,15 +2,15 @@ class SearchController < ApplicationController
   def index
     wildcard_search = "%#{params[:keywords]}%"
 
-    if params[:dropdown_selection] == "anime"
+    if params[:dropdown_selection] == "Anime"
       @searches = Anime.where("name LIKE ?", wildcard_search)
-    elsif params[:dropdown_selection] == "manga"
+    elsif params[:dropdown_selection] == "Manga"
       @searches = Anime.where("name LIKE ?", wildcard_search)
-    elsif params[:dropdown_selection] == "genre"
+    elsif params[:dropdown_selection] == "Genre"
       @searches = Genre.where("name LIKE ?", wildcard_search)
-    elsif params[:dropdown_selection] == "studio"
+    elsif params[:dropdown_selection] == "Studio"
       @searches = Studio.where("name LIKE ?", wildcard_search)
-    elsif params[:dropdown_selection] == "producer"
+    elsif params[:dropdown_selection] == "Producer"
       @searches = Producer.where("name LIKE ?", wildcard_search)
     end
   end
